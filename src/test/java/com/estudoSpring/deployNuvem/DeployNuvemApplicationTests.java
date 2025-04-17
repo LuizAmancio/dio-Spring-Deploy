33df85a0409142d25d870cea15bc57e1f055b6f8
LuizAmancio <luizfelipeamancio@gmail.com>
2025-04-17 19:27:52 -0300

    Spring deploy na nuvem

---
+++ src/test/java/com/estudoSpring/deployNuvem/DeployNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoSpring.deployNuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
